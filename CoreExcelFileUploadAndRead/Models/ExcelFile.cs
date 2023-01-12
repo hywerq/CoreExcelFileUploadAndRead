8514c30eebe7e813aa8b54f5e292668ba938d6ba
hywerq <hywerq@gmail.com>
2023-01-12 21:34:22 +0300

    Added entities for every file component.

--- CoreExcelFileUploadAndRead/Models/ExcelFile.cs
+++ CoreExcelFileUploadAndRead/Models/ExcelFile.cs
@@ -5,17 +5,18 @@
         public int Id { get; set; }
         public string Name { get; set; } = "";
         public string Title { get; set; } = "";
+        public string BankName { get; set; } = "";
         public string Subject { get; set; } = "";
         public string Currency { get; set; } = "";
         public DateTime PrintTime { get; set; }
         public DateTime UploadTime { get; set; }
         public DateTime PeriodStart { get; set; } 
         public DateTime PeriodEnd { get; set; }
-        public double OpeningBalanceActive { get; set; }
-        public double OpeningBalancePassive { get; set; }
-        public double TurnoverDebit { get; set; }
-        public double TurnoverCredit { get; set; }
-        public double ClosingBalanceActive { get; set; }
-        public double ClosingBalancePassive { get; set; }
+        public decimal OpeningBalanceActive { get; set; }
+        public decimal OpeningBalancePassive { get; set; }
+        public decimal TurnoverDebit { get; set; }
+        public decimal TurnoverCredit { get; set; }
+        public decimal ClosingBalanceActive { get; set; }
+        public decimal ClosingBalancePassive { get; set; }
     }
 }
