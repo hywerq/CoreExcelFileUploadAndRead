dfacf269bbde27bcf0c2b8d9c7933f3bcda94970
hywerq <hywerq@gmail.com>
2023-01-12 21:34:22 +0300

    Added entities for every file component.

---
+++ CoreExcelFileUploadAndRead/Models/Content/FileData.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreExcelFileUploadAndRead.Models.Content
+{
+	public class FileData
+	{
+        public List<ExcelFile> Files { get; set; } = new();
+        public List<Class> Classes { get; set; } = new();
+        public List<ClassGroup> ClassGroups { get; set; } = new();
+        public List<BalanceAccount> BalanceAccounts { get; set; } = new();
+    }
+}
---
+++ CoreExcelFileUploadAndRead/Models/ExcelFileReader.cs
@@ -0,0 +1,10 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CoreExcelFileUploadAndRead.Models
+{
+    public static class ExcelFileReader
+	{
+
+    }
+}
